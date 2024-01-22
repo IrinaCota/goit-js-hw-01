d08256e3dee01119ee139113b88ddca66538ad58
IrinaCota <iryna.kotsa91@gmail.com>
2024-01-22 20:15:49 +0000

    format with prettier

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,10 @@
 function getElementWidth(content, padding, border) {
-    const contentVal = Number.parseFloat(content);
-    const paddingVal = Number.parseFloat(padding);
-    const borderVal = Number.parseFloat(border);
+  const contentVal = Number.parseFloat(content);
+  const paddingVal = Number.parseFloat(padding);
+  const borderVal = Number.parseFloat(border);
 
-    return contentVal + paddingVal * 2 + borderVal * 2;
+  return contentVal + paddingVal * 2 + borderVal * 2;
 }
-
 
 console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px"));
